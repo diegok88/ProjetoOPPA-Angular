9e300529f8752e71c3b1b8ebe3d338b3d8684cc3
diegok88 <ddkotz@minha.fag.edu.br>
2026-07-05 06:57:34 -0300

    Iniciado o dashboard adimin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardAdmin } from './dashboard/dashboard-admin/dashboard-admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard-admin',
+    component: DashboardAdmin,
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard-admin',
+    pathMatch: 'full',
+  },
+];
