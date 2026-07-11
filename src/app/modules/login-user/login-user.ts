17dcb2dfd529bedce8ea501414148ab3a920263e
diegok88 <https://github.com/diegok88>
2026-07-10 21:31:49 -0300

    Ajustado tela de login

--- src/app/modules/login-user/login-user.ts
+++ src/app/modules/login-user/login-user.ts
@@ -8,8 +8,24 @@ import { LoginData } from '../../../interfaces/login-data.interface';
   styleUrl: './login-user.scss',
 })
 export class LoginUser {
+  // Objeto armazena dados para o login
   protected loginModel = signal<LoginData>({
     badge: '',
     password: '',
   });
+  // Variavel de erro
+  protected badgeError = signal<boolean>(false);
+
+  // Função para verificar apenas numeros
+  protected onBadgeInput(value: string) {
+    // Remove tudo que não é número
+    const numberOnly = value.replace(/\D/g, '');
+    // Verifica se houve alteração
+    this.badgeError.set(value !== numberOnly);
+    // Atualiza o objeto
+    this.loginModel.update((model) => ({
+      ...model,
+      badge: numberOnly,
+    }));
+  }
 }
