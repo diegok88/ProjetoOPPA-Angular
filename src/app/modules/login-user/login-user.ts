dcc5bec14ce75d565b26f552af8d3d106224d8c7
diegok88 <ddkotz@minha.fag.edu.br>
2026-07-11 04:39:39 -0300

    Finalizado requisito login no front

--- src/app/modules/login-user/login-user.ts
+++ src/app/modules/login-user/login-user.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { LoginData } from '../../../interfaces/login-data.interface';
 
 @Component({
@@ -8,24 +8,57 @@ import { LoginData } from '../../../interfaces/login-data.interface';
   styleUrl: './login-user.scss',
 })
 export class LoginUser {
-  // Objeto armazena dados para o login
-  protected loginModel = signal<LoginData>({
-    badge: '',
-    password: '',
+  protected loginModel = signal<LoginData>({ badge: '', password: '' }); // Objeto armazena dados para o login
+  protected badgeErrorNumber = signal<boolean>(false); // armazena valor boleano se contem ou não numeros
+  protected badgeEmptyFieldError = signal<boolean>(false); // armazena valor boleano se esta vazio
+  protected passwordEmptyFieldError = signal<boolean>(false); // armazena valor boleano se esta vazio
+  protected showPassword = signal<boolean>(false); // armazena valor boleano para vizualização da senha
+  // Validação geral somente ao enviar o post do login
+  protected isFormValid = computed(() => {
+    const model = this.loginModel();
+    const badgeOk = model.badge.length > 0 && !this.badgeErrorNumber();
+    const passwordOk = model.password.length > 0 && !this.passwordEmptyFieldError();
+    return badgeOk && passwordOk;
   });
-  // Variavel de erro
-  protected badgeError = signal<boolean>(false);
 
-  // Função para verificar apenas numeros
-  protected onBadgeInput(value: string) {
-    // Remove tudo que não é número
-    const numberOnly = value.replace(/\D/g, '');
-    // Verifica se houve alteração
-    this.badgeError.set(value !== numberOnly);
+  // Função para verificar se é numero ou vazio
+  protected onBadgeInput(value: string): void {
+    // Função de validação da variavel badge
+    this.validateBadge(value);
     // Atualiza o objeto
-    this.loginModel.update((model) => ({
-      ...model,
-      badge: numberOnly,
-    }));
+    this.loginModel.update((model) => ({ ...model, badge: value }));
+  }
+  // Função de verificação se vazio
+  protected onPasswordInput(value: string): void {
+    this.validatePassword(value);
+    this.loginModel.update((model) => ({ ...model, password: value }));
+  }
+  // Funções de validações
+  private validateBadge(value: string): void {
+    const onlyNumbers = /^\d+$/.test(value);
+    // Retorno boleano que verifica se é numero e maior que 0
+    this.badgeErrorNumber.set(!onlyNumbers && value.length > 0);
+    // Retorno boleano se o campo estiver vazio
+    this.badgeEmptyFieldError.set(value.trim().length === 0);
+  }
+  private validatePassword(value: string): void {
+    // trim() - elimina os espaçamentos
+    // length - retorna a quantidade de caracteres
+    this.passwordEmptyFieldError.set(value.trim().length === 0);
+  }
+  // Função de visualização da senha
+  protected togglePasswordVisibility(): void {
+    this.showPassword.update((value) => !value);
+  }
+  // Função de confirmação do login
+  protected onSubmit(): void {
+    // Revalida para segurança
+    this.validateBadge(this.loginModel().badge);
+    this.validatePassword(this.loginModel().password);
+    if (!this.isFormValid()) {
+      console.warn('Formulário inválido - não enviar');
+      return;
+    }
+    console.log('Login com: ', this.loginModel());
   }
 }
