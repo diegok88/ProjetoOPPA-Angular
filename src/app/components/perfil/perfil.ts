94b108d671253f2cb3796f4ec165e1a22df09df0
diegok88 <https://github.com/diegok88>
2026-08-12 22:33:57 -0300

    update register profile

--- src/app/components/perfil/perfil.ts
+++ src/app/components/perfil/perfil.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PerfilService } from '../../services/perfil.service';
+import { PerfilData } from '../../interfaces/perfil-data.interface';
 
 type Operacao = 'inicial' | 'cadastrar' | 'registro';
-type Registro = 'informacao' | 'atualizar' | 'inativar' | 'eliminar';
+type Registro = 'informacao' | 'atualizar' | 'inativar' | 'eliminar' | 'auditoria';
 
 @Component({
   selector: 'app-perfil',
@@ -14,6 +15,7 @@ type Registro = 'informacao' | 'atualizar' | 'inativar' | 'eliminar';
 export class Perfil implements OnInit {
   private perfilService = inject(PerfilService);
   protected readonly listar = this.perfilService.perfil;
+  protected readonly buscar = signal<PerfilData | null>(null);
 
   protected operacaoEstado = signal<string>('inicial');
   protected registroEstado = signal<string>('informacao');
@@ -26,8 +28,16 @@ export class Perfil implements OnInit {
     this.perfilService.listar();
   }
 
-  protected mudarOperacao(operacao: Operacao): void {
+  protected mudarOperacao(operacao: Operacao, item?: PerfilData): void {
+    if (operacao === 'registro') {
+      this.registroEstado.set('informacao');
+      if (item) this.carregarRegistro(item);
+    }
     this.operacaoEstado.set(operacao);
+  }
+
+  private carregarRegistro(item: PerfilData): void {
+    this.buscar.set(item);
   }
 
   protected mudarRegistro(registro: Registro): void {
